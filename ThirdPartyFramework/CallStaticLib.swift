d3248d9bc1f13824a359ffd241b2f2054ebd3ff3
Rahul Singh <rahulsingh@nocs-MacBook-Pro.local>
2021-07-29 15:18:59 +0530

    First changes

---
+++ ThirdPartyFramework/CallStaticLib.swift
@@ -0,0 +1,19 @@
+//
+//  CallStaticLib.swift
+//  TestFramework
+//
+//  Created by Rahul Singh on 29/07/21.
+//
+
+import Foundation
+import MyStaticLib
+
+public class CallStaticLib: NSObject {
+    public override init() {
+        super.init()
+    }
+    
+    public func callStaticLib() {
+        functionA()
+    }
+}
